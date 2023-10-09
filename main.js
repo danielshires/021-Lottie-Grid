672300b7a223725037bdbf583b586d3eba14a293
danielshires <hello@danielshires.com>
2023-10-09 13:06:41 +1100

    Initial set-up. Added CSS styling and JS. Added initial lotties

--- main.js
+++ main.js
@@ -1,24 +1,66 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import lottie from 'lottie-web';
+import "@lottiefiles/lottie-player";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const containerGrid = document.querySelector('.lottie-grid')
+const header = document.querySelector('.flex')
 
-setupCounter(document.querySelector('#counter'))
+const cursor = document.createElement('div')
+
+cursor.className = "cursor"
+// document.body.appendChild(cursor)
+
+document.body.insertBefore(cursor, header);
+
+
+const createDivs = ((num) => {
+    console.log("Create Divs running")
+    for (let i = 1; i < num; i++) {
+        let gridItem = document.createElement('div')
+        gridItem.className = `_${i}`
+        containerGrid.append(gridItem)
+    }
+})
+
+createDivs(60)
+
+
+let lottie_1 = document.querySelector("._1");
+let lottie_2 = document.querySelector("._2");
+
+const lottieAnimation_1 = lottie.loadAnimation({
+    container: lottie_1,
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: './lotties/01/dotty-lottie-01.json',
+});
+const lottieAnimation_2 = lottie.loadAnimation({
+    container: lottie_2,
+    renderer: 'svg',
+    loop: true,
+    autoplay: false,
+    path: './lotties/02/dotty-lottie-02.json',
+});
+
+lottie_1.addEventListener('click', function () {
+    lottieAnimation_1.play()
+});
+
+lottie_2.addEventListener('click', function () {
+    lottieAnimation_2.play()
+});
+
+
+
+//   Cursor
+
+
+const moveCursor = (e) => {
+    const mouseY = e.pageY;
+    const mouseX = e.pageX;
+    cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+}
+
+window.addEventListener('mousemove', moveCursor)
+window.addEventListener("scroll", moveCursor);
